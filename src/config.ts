32aafdf1c0c3df1d776167cc3e977c364037e9c7
altrabot <altrabot17@gmail.com>
2025-09-05 10:38:28 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,8 @@ export const config = {
   port: process.env.PORT || 3000,
   ownerNumber: process.env.OWNER_NUMBER || '083131871328',
   sessionDir: process.env.SESSION_DIR || './sessions',
-  databaseUrl: process.env.DATABASE_URL || 'postgresql://user:pass@localhost:5432/altrabot'
+  databaseUrl: process.env.DATABASE_URL || 'postgres://7c8228d94725305c4fee7b2874d2d7c6ed03c568c69e375b42173e313f37287e:sk_K4KFOB900HrkeqoYGgVEa@db.prisma.io:5432/postgres?sslmode=require',
+  redisUrl: process.env.REDIS_URL,
+  baseUrl: process.env.BASE_URL,
+  qrImageUrl: process.env.QR_IMAGE_URL || 'https://files.catbox.moe/311wcy.jpeg'
 };
